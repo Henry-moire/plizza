51c6c555022db6f9df44dd73ada406c9e0cd1bcb
Henry <henrymoire@gmail.com>
2020-04-20 16:35:28 +0300

    Made the labels toggle the choices section

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,39 @@
 $(document).ready(function() {
   $(".magheritaClickable").click(function() {
-    $("#designShowing").toggle();
-    $("#designHidden").toggle();
+    $(".choices").toggle();
+    $("#magheritaButton").show();
+    $("#mushroomButton").hide();
+    $("#pepperoniButton").hide();
+    $("#vegeterianButton").hide();
+  });
+});
+
+$(document).ready(function() {
+  $(".mushroomClickable").click(function() {
+    $(".choices").toggle();
+    $("#magheritaButton").hide();
+    $("#mushroomButton").show();
+    $("#pepperoniButton").hide();
+    $("#vegeterianButton").hide();
+  });
+});
+
+$(document).ready(function() {
+  $(".pepperoniClickable").click(function() {
+    $(".choices").toggle();
+    $("#magheritaButton").hide();
+    $("#mushroomButton").hide();
+    $("#pepperoniButton").show();
+    $("#vegeterianButton").hide();
+  });
+});
+
+$(document).ready(function() {
+  $(".vegeterianClickable").click(function() {
+    $(".choices").toggle();
+    $("#magheritaButton").hide();
+    $("#mushroomButton").hide();
+    $("#pepperoniButton").hide();
+    $("#vegeterianButton").show();
   });
 });
